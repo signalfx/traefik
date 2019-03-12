0a552d2023d039de4734d17a3d5b7a152530d986
Ryan Fitzpatrick <rmfitzpatrick@signalfx.com>
2019-03-12 19:15:46 -0400

    Add Jaeger Collector Endpoint and pull in improvements

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -340,6 +340,8 @@ func (gc *GlobalConfiguration) initTracing() {
 					SamplingType:           "const",
 					SamplingParam:          1.0,
 					LocalAgentHostPort:     "127.0.0.1:6831",
+					Propagation:            "jaeger",
+					Gen128Bit:              false,
 					TraceContextHeaderName: jaegercli.TraceContextHeaderName,
 				}
 			}
